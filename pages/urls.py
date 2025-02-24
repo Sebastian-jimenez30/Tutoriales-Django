4acdce98103c800ac3c213722c5297bb9b3839ba
Sebastian Jimenez <anderjime864@gmail.com>
2025-02-23 19:18:44 -0500

    Tutorial dependencias

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from pages.utils import ImageLocalStorage
+
 
 urlpatterns =[
     path("",HomePageView.as_view(), name='home'),
@@ -11,5 +13,7 @@ urlpatterns =[
     path('products/<str:id>',ProductShowView.as_view(),name='show'),
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),  
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
 ]   
